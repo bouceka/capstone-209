ad30d930501428f9ceaf0e19079e9ac270e14128
Adam Boucek <adamm01b@gmail.com>
2022-11-28 22:15:16 -0800

    feat: Create link button

---
+++ nic-athletics-spa/src/components/link-button/link-button.component.tsx
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+import './link-button.styles.scss';
+
+interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: React.ReactNode;
+  size: 'small' | 'medium';
+}
+export const LinkButton = ({ children, size, ...props }: Props) => {
+  return (
+    <a href="#!" {...props} className={`link paragraph--${size} ${props.className}`}>
+      {children}
+    </a>
+  );
+};
