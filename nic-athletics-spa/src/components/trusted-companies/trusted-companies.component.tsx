4a0492121489c26ff694df11644a14efec9c8093
Adam Boucek <adamm01b@gmail.com>
2022-11-28 22:17:45 -0800

    feat: Create trusted company component

---
+++ nic-athletics-spa/src/components/trusted-companies/trusted-companies.component.tsx
@@ -0,0 +1,29 @@
+// @flow
+import * as React from 'react';
+import './trusted-companies.styles.scss';
+type Props = {};
+export const TrustedCompanies = (props: Props) => {
+  return (
+    <section className='trusted-companies'>
+      <div className='row'>
+        <div className='trusted-companies__group'>
+          <div className='trusted-companies__item'>
+            <img src='/src/assets/logos/Name=Delta.png' alt='Delta airlines logo' />
+          </div>
+          <div className='trusted-companies__item'>
+            <img src='/src/assets/logos/Name=Doordash.png' alt='Doordash logo' />
+          </div>
+          <div className='trusted-companies__item'>
+            <img src='/src/assets/logos/Name=Gladstone.png' alt='Gladstone logo' />
+          </div>
+          <div className='trusted-companies__item'>
+            <img src='/src/assets/logos/Name=Crown Isle.png' alt='Crown Isle logo' />
+          </div>
+          <div className='trusted-companies__item'>
+            <img src='/src/assets/nic-logo.png' alt='NIC logo' />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
