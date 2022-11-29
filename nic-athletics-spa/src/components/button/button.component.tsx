39188977dfadca4288dcdfc57f95ad39ed217783
Adam Boucek <adamm01b@gmail.com>
2022-11-28 22:14:36 -0800

    feat: Add button code

---
+++ nic-athletics-spa/src/components/button/button.component.tsx
@@ -0,0 +1,13 @@
+// @flow
+import * as React from 'react';
+import "./button.styles.scss"
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: React.ReactNode;
+  }
+export const Button = ({children, ...props}: Props) => {
+	return (
+		<button {...props} className={`btn btn--${props.className}`} >
+			{children}
+		</button>
+	);
+};
