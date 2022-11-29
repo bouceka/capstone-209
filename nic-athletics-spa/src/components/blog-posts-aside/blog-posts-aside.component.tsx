e2e43c3c0c41b857d1737ea42ca7fab36926f079
Adam Boucek <adamm01b@gmail.com>
2022-11-28 22:15:02 -0800

    feat: Create blog posts aside

---
+++ nic-athletics-spa/src/components/blog-posts-aside/blog-posts-aside.component.tsx
@@ -0,0 +1,32 @@
+import { format } from 'date-fns';
+import * as React from 'react';
+import { CAROUSEL_DATA } from '../carousel/mock-carousel.data';
+import './blog-post-aside.styles.scss';
+export interface Props {}
+
+export function BlogPostsAside(props: Props) {
+  return (
+    <aside className="blog-posts-aside">
+      {CAROUSEL_DATA.map(
+        (post, index) =>
+          index > 0 && (
+            <div key={index} className="blog-posts-aside__item">
+              <img src={post.featuredImage} alt="" />
+              <div className="blog-posts-aside__text">
+                <h6>{post.title}</h6>
+                <div className="blog-posts-aside__overline">
+                  <span className="blog-posts-aside__date">
+                    {format(new Date(), 'MMM dd, yyyy')}
+                  </span>
+                  <span className="blog-posts-aside__divider"></span>
+                  <span className="blog-posts-aside__category">
+                    {post.category}
+                  </span>
+                </div>
+              </div>
+            </div>
+          )
+      )}
+    </aside>
+  );
+}
