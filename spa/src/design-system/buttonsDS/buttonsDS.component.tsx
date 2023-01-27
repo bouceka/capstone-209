edb985929426c17dabf14c4e06af7eef14d567de
Adam Boucek <adamm01b@gmail.com>
2023-01-27 15:50:49 -0800

    chore: Generalize element settings

--- spa/src/design-system/buttonsDS/buttonsDS.component.tsx
+++ spa/src/design-system/buttonsDS/buttonsDS.component.tsx
@@ -1,29 +1,19 @@
 // @flow
 import * as React from 'react';
 import { Button } from '../../components/button/button.component';
-import { LinkButton } from '../../components/link-button/link-button.component';
-import Highlight, { defaultProps } from 'prism-react-renderer';
 import { CodeSnippet } from '../code-snippet/code-snippet.component';
 import { ButtonCSSSample, ButtonExampleSample, ButtonTSXSample } from './button-sample';
 
-const exampleCode = `
-(function someDemo() {
-  var test = "Hello World!";
-  console.log(test);
-})();
-
-return () => <App />;
-`;
 
 type Props = {};
 export const ButtonsDS = (props: Props) => {
   return (
-    <section id='button-ds' className='button-ds'>
+    <section id='button-ds' className='element-ds'>
       <h2 className='heading'>Buttons</h2>
       <p className='paragraph--large'>
         Use NIC Athletics’s custom buttons styles for any typo of component such as forms, cards, CTA and more.{' '}
       </p>
-      <div className='button-ds__examples'>
+      <div className='element-ds__examples buttons'>
         <Button className='primary'>Button Primary</Button>
         <Button className='primary disabled'>Primary Disabled</Button>
         <Button className='secondary'>Button Secondary</Button>
