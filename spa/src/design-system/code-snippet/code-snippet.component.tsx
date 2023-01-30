1680fd6e292b745cb5c243d877b30568811edfe5
Adam Boucek <adamm01b@gmail.com>
2023-01-29 16:53:23 -0800

    feat: Chnage code snippet dependancy

--- spa/src/design-system/code-snippet/code-snippet.component.tsx
+++ spa/src/design-system/code-snippet/code-snippet.component.tsx
@@ -3,29 +3,31 @@
 import * as React from 'react';
 import { HighlightedCode } from './highlighted-code.component';
 type Props = {
-  TSXCode: string;
+  TSXCode?: string;
   CSSCode: string;
   exampleCode: string;
 };
-export const CodeSnippet = ({ TSXCode, CSSCode, exampleCode }: Props) => {
+export const CodeSnippet = ({ TSXCode = '', CSSCode, exampleCode }: Props) => {
   return (
     <div className='code-snippet'>
-      <div className='code-snippet__TSX'>
-        <div className='code-snippet__label'>TSX Code</div>
-        <div className='code-snippet__content'>
-          <HighlightedCode lang='tsx' code={TSXCode} />
+      {TSXCode ? (
+        <div className='code-snippet__TSX'>
+          <div className='code-snippet__label'>TSX Code</div>
+          <div className='code-snippet__content'>
+            <HighlightedCode lang='tsx' code={TSXCode} />
+          </div>
         </div>
-      </div>
+      ) : null}
       <div className='code-snippet__CSS'>
         <div className='code-snippet__label'>SCSS Code</div>
         <div className='code-snippet__content'>
-        <HighlightedCode lang='scss' code={CSSCode} />
+          <HighlightedCode lang='scss' code={CSSCode} />
         </div>
       </div>
       <div className='code-snippet__example'>
         <div className='code-snippet__label'>Example Usage</div>
         <div className='code-snippet__content'>
-        <HighlightedCode lang='tsx' code={exampleCode} />
+          <HighlightedCode lang='tsx' code={exampleCode} />
         </div>
       </div>
     </div>
