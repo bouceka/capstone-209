bd57b287eb01c2770f473ad5a253c0b060c48f66
Adam Boucek <adamm01b@gmail.com>
2023-01-29 17:30:25 -0800

    feat: Add typography table

---
+++ spa/src/design-system/typography/typography-table.component.tsx
@@ -0,0 +1,55 @@
+// @flow
+import * as React from 'react';
+import { TypographySampleProps } from './typography-sample';
+
+
+
+type Props = {
+  typographyList: TypographySampleProps[];
+};
+export const TypographyTable = ({ typographyList }: Props) => {
+  return (
+    <section className='typography-ds__box'>
+      <table className='typography-ds__table'>
+        <thead>
+          <tr>
+            <th>
+              <span className='paragraph--bold'>Sample Text</span>
+            </th>
+            <th>
+              <span className='paragraph--bold'>Typeface</span>
+            </th>
+            <th>
+              <span className='paragraph--bold'>Weight</span>
+            </th>
+            <th>
+              <span className='paragraph--bold'>Size</span>
+            </th>
+            <th>
+              <span className='paragraph--bold'>Height</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {typographyList.map(({ typeface, weight, size, height, className,content }, index) => (
+            <tr key={index}>
+              <td dangerouslySetInnerHTML={{ __html: content }}></td>
+              <td>
+                <p className='paragraph--medium'>{typeface}</p>
+              </td>
+              <td>
+                <p className='paragraph--medium'>{weight}</p>
+              </td>
+              <td>
+                <p className='paragraph--medium'>{size}</p>
+              </td>
+              <td>
+                <p className='paragraph--medium'>{height}</p>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
