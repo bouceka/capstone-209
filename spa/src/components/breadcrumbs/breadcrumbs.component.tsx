e2c7a826e1768e3535ab4251fdf6e72550246956
Adam Boucek <adamm01b@gmail.com>
2023-01-31 11:14:29 -0800

    feat: Create breadcrumbs

---
+++ spa/src/components/breadcrumbs/breadcrumbs.component.tsx
@@ -0,0 +1,43 @@
+// @flow
+import * as React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { LinkButton } from '../link-button/link-button.component';
+type Props = {};
+
+const capitalize = (word: string) => word.charAt(0).toUpperCase() + word.slice(1);
+
+const styleCrumb = (crumb: string) => {
+  return capitalize(crumb.replace('-', ' '));
+};
+export const Breadcrumbs = (props: Props) => {
+  const location = useLocation();
+
+  let currentLink = '';
+
+  const crumbs = location.pathname
+    .split('/')
+    .filter((crumb) => crumb !== '')
+    .map((crumb) => {
+      currentLink += `/${crumb}`;
+      return (
+        <div className='crumb' key={crumb}>
+          <LinkButton size='medium' to={currentLink}>
+            {styleCrumb(crumb)}
+          </LinkButton>
+        </div>
+      );
+    });
+
+  crumbs.unshift(
+    <div className='crumb' key='home'>
+      <LinkButton size='medium' to={'/'}>
+        Home
+      </LinkButton>
+    </div>
+  );
+  return (
+    <div>
+      <div className='row breadcrumbs'>{crumbs}</div>
+    </div>
+  );
+};
