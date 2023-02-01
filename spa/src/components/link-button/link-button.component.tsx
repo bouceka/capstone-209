02f534b3518418368199486cb45099549f9a76a0
Adam Boucek <adamm01b@gmail.com>
2023-02-01 11:05:35 -0800

    feat: Add size o link button

--- spa/src/components/link-button/link-button.component.tsx
+++ spa/src/components/link-button/link-button.component.tsx
@@ -4,10 +4,10 @@ import './link-button.styles.scss';
 
 interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode;
-  size: 'small' | 'medium';
+  size?: 'small' | 'medium' | 'large';
   to?: string;
 }
-export const LinkButton = ({ children, size, to='#!', ...props }: Props) => {
+export const LinkButton = ({ children, size = 'medium', to = '#!', ...props }: Props) => {
   return (
     <a href={to} {...props} className={`link paragraph--${size} ${props.className}`}>
       {children}
