16f778c0d0b712680c0ad757462ccc75400ba217
bouceka <adamm01b@gmail.com>
2023-02-13 18:46:21 -0800

    feat: Add border radius rule

--- spa/src/design-system/breadcrumbsDS/breadcrumbsDS.component.tsx
+++ spa/src/design-system/breadcrumbsDS/breadcrumbsDS.component.tsx
@@ -8,7 +8,7 @@ export const BreadcrumbsDS = (props: Props) => {
   return (
     <section className='breadcrumbs-ds' id='breadcrumbs-ds'>
       <h4 className='heading'>Breadcrumbs</h4>
-      <p className='paragraph--large'>
+      <p className='paragraph--medium'>
         Breadcrumbs is a very simple component what is created from links. It can be used anywhere on the page and on
         any page. It doesn't need any property because it gets all the information from the current URL. It always
         displays home and then the address.
--- spa/src/design-system/sapcingDS/spacingDS.component.tsx
+++ spa/src/design-system/sapcingDS/spacingDS.component.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { LinkButton } from '../../components/link-button/link-button.component';
 import SpacingImg from '../../assets/illustrative-img/8px-rule.png';
+import RadiusImg from '../../assets/illustrative-img/border-radius.png';
 import { Highlight } from '../highlight/highlight.component';
 
 type Props = {};
@@ -29,16 +30,30 @@ export const SpacingDS = (props: Props) => {
         <LinkButton to='https://www.freecodecamp.org/news/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862/'>
           "The 100% correct way to do CSS breakpoints"
         </LinkButton>
-        . In the this project I use the relative unit <Highlight>font-size: 62.5%</Highlight> that is <Highlight>1rem = 16px</Highlight>{' '}
-        with various ration in each breakpoint. The break points are:
+        . In the this project I use the relative unit <Highlight>font-size: 62.5%</Highlight> that is{' '}
+        <Highlight>1rem = 16px</Highlight> with various ration in each breakpoint. The break points are:
       </p>
       <ul>
         <li>Phone (0px - 599px)</li>
-        <li>Tablet Portrait (600px - 899px) <Highlight>font-size: 50%; 1rem = 8px</Highlight></li>
-        <li>Tablet Landscape (900px - 1199px) <Highlight>font-size: 56.25%; 1rem = 9px</Highlight></li>
-        <li>Desktop (1200px - 1799px) <Highlight>font-size: 62.5%; 1rem = 16px</Highlight></li>
-        <li>Large Desktop (1800px and up) <Highlight>font-size: 75%; 1rem = 12px</Highlight></li>
+        <li>
+          Tablet Portrait (600px - 899px) <Highlight>font-size: 50%; 1rem = 8px</Highlight>
+        </li>
+        <li>
+          Tablet Landscape (900px - 1199px) <Highlight>font-size: 56.25%; 1rem = 9px</Highlight>
+        </li>
+        <li>
+          Desktop (1200px - 1799px) <Highlight>font-size: 62.5%; 1rem = 16px</Highlight>
+        </li>
+        <li>
+          Large Desktop (1800px and up) <Highlight>font-size: 75%; 1rem = 12px</Highlight>
+        </li>
       </ul>
+      <h6 className='heading'>Border Radius</h6>
+      <img src={RadiusImg} alt='border radius rule' />
+      <p className='paragraph--medium'>
+        For most of the UI elements I am using 5px border radius for outer border (larger elements) and for the inner
+        elements I am using 2px border radius.
+      </p>
     </section>
   );
 };
