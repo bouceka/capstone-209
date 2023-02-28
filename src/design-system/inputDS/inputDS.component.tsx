c21ed7d05bac99793d7874f6dec7fa06629e6117
Adam Boucek <adamm01b@gmail.com>
2023-02-28 15:46:10 -0800

    chore: Add input example into DS

--- src/design-system/inputDS/inputDS.component.tsx
+++ src/design-system/inputDS/inputDS.component.tsx
@@ -6,6 +6,7 @@ import { InputCSSSample, InputExampleSample, InputTSXSample } from './input-samp
 import { Highlight } from '../highlight/highlight.component';
 import inputGrid from '../../assets/illustrative-img/input-grid.png';
 import { LinkButton } from '../../components/link-button/link-button.component';
+import { Button } from '../../components/button/button.component';
 type Props = {};
 export const InputDS = (props: Props) => {
   return (
@@ -15,7 +16,8 @@ export const InputDS = (props: Props) => {
         Inputs can be used for any types of form. Every input has label, help text, and the field. If the field type is
         not specified, it default sets type <Highlight>text</Highlight>. Inserting help text and label text is optional,
         however, it is highly recommended to use <Highlight>label</Highlight>. The input field is high{' '}
-        <Highlight>40rem</Highlight>{', '}
+        <Highlight>40rem</Highlight>
+        {', '}
         <LinkButton to='https://uxplanet.org/designing-perfect-text-field-clarity-accessibility-and-user-effort-d03c1e26004b'>
           based on recommended height between 32px and 40px.
         </LinkButton>
@@ -25,11 +27,40 @@ export const InputDS = (props: Props) => {
         <Input label='Input label' helpText='Add help text' placeholder='Placeholder' />
       </div>
 
+      <h6 className='heading'>Example</h6>
+      <section className='contact-page'>
+        <div className='contact-page__wrapper'>
+          <h2 className='heading u-center-text'>NIC Athletics - Registration</h2>
+          <p className='paragraph--medium u-center-text'>Sign up for the fall season 2022</p>
+          <form action=''>
+            <div className='form-group'>
+              <Input label='First Name' name='firstName' placeholder='John' />
+              <Input label='Last Name' name='lastName' placeholder='Doe' />
+            </div>
+            <div className='form-group'>
+              <Input label='North Island College Email' name='email' placeholder='john.doe@example.com' />
+            </div>
+            <div className='form-group'>
+              <Input label='Password' type='password' name='password' placeholder='Password' />
+            </div>
+            <div className='form-group'>
+              <Input label='School Number' name='schoolNumber' placeholder='n0123456' />
+            </div>
+            <div className='form-group'>
+              <Input label='Sport Type' name='email' placeholder='Basketball, Volleyball' />
+            </div>
+            <p className='caption--medium'>
+              This form is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.
+            </p>
+            <Button className='primary'>Submit</Button>
+          </form>
+        </div>
+      </section>
       <h6 className='heading'>Usage</h6>
       <p className='paragraph--medium'>
         A developer can use an input as <span className='paragraph--medium--bold'>JSX tag</span> in syntax:{' '}
-        <Highlight>{`<Input label='Input label' type='text' placeholder='Placeholder' />`}</Highlight>. You can treat the input tag as
-        a regular button when you can pass any attributes that belong to HTML input.
+        <Highlight>{`<Input label='Input label' type='text' placeholder='Placeholder' />`}</Highlight>. You can treat
+        the input tag as a regular button when you can pass any attributes that belong to HTML input.
       </p>
       <CodeSnippet TSXCode={InputTSXSample} exampleCode={InputExampleSample} SCSSCode={InputCSSSample} />
     </section>
