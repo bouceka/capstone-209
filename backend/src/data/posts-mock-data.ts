3f9735e1568cba748689dcb104886a8983a6ed72
Adam Boucek <adamm01b@gmail.com>
2023-03-18 17:14:55 -0700

    feat: Resize big images

--- backend/src/data/posts-mock-data.ts
+++ backend/src/data/posts-mock-data.ts
@@ -12,7 +12,7 @@ export const POSTS_DATA: BlogPost[] = [
     id: '1',
     title: 'Soccer kicks off spring at HIGHLAND Field tonight',
     category: "Men's Soccer",
-    featuredImage: '/assets/images/tony-bustamante-o98lg_hUHqY-unsplash.jpeg',
+    featuredImage: '/assets/images/tony-bustamante-o98lg_hUHqY-unsplash.jpg',
     url: 'https://images.unsplash.com/photo-1516463439953-eb6a1c2867b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
     content:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iusto dolore delectus aliquam est illo consectetur iste repellendus dolores nemo odit enim nobis distinctio rerum perferendis nihil labore voluptate modi.',
@@ -21,7 +21,7 @@ export const POSTS_DATA: BlogPost[] = [
     id: '2',
     title: 'NIC Women’s Volleyball Defeats UBC',
     category: "Women's Volleyball",
-    featuredImage: '/assets/images/vince-fleming-aZVpxRydiJk-unsplash.jpeg',
+    featuredImage: '/assets/images/vince-fleming-aZVpxRydiJk-unsplash.jpg',
     url: 'https://images.unsplash.com/photo-1547347298-4074fc3086f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
     content:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iusto dolore delectus aliquam est illo consectetur iste repellendus dolores nemo odit enim nobis distinctio rerum perferendis nihil labore voluptate modi.',
@@ -30,7 +30,7 @@ export const POSTS_DATA: BlogPost[] = [
     id: '3',
     title: 'Novak’s Late Goal Ends NIC’s Season With 1-0 Win',
     category: "Men's Soccer",
-    featuredImage: '/assets/images/kenny-webster-JqCpvGN0JFo-unsplash.jpeg',
+    featuredImage: '/assets/images/kenny-webster-JqCpvGN0JFo-unsplash.jpg',
     url: 'https://images.unsplash.com/photo-1518657205760-dafa7ef924de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
     content:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, iusto dolore delectus aliquam est illo consectetur iste repellendus dolores nemo odit enim nobis distinctio rerum perferendis nihil labore voluptate modi.',
